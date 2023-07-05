77f01c413fe98eb63bab4db7b7a87003d8c78c4c
MarianaSanchezGonzalez03 <mari98san33@gmail.com>
2023-07-05 13:32:40 -0600

    Clase 4

--- lib/main.dart
+++ lib/main.dart
@@ -6,28 +6,35 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Water Counter',
+      title: 'Water counter',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-       scaffoldBackgroundColor: Colors.black,
-       primaryColor: Colors.indigo,
-       textTheme:
-       const TextTheme(
-        headlineSmall: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold),
-        bodyMedium: TextStyle(
-          color: Color.fromARGB(255, 1376, 186, 243)
-        )
-
-       ),
-      useMaterial3: true,
+        scaffoldBackgroundColor: Colors.black,
+        primaryColor: Colors.indigo,
+        textTheme: const TextTheme(
+          headlineSmall:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          bodyMedium: TextStyle(
+            color: Color.fromARGB(255, 127, 186, 243),
+          ),
+          bodySmall: TextStyle(
+            color: Colors.white, 
+            fontSize: 12,
+            fontWeight: FontWeight.bold),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.indigo,
+            foregroundColor: Colors.white,
+          ),
+        ),
+        useMaterial3: true,
       ),
       home: const StartScreen(),
     );
--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -44,27 +44,91 @@ class _StartScreenState extends State<StartScreen>{
 
   }
   Widget _indicators(){
-    return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  '0 %',
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
-                Text(
+    return IntrinsicHeight( //elementos verticales
+      child:
+           Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _percentage(),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  _smallDivider(),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  _hydration(),
+                ],
+              ),
+    );
+  }
+
+  Widget _percentage(){
+    return Column(
+      children: [
+          SizedBox(
+            height:60,
+            width: 40,
+            child: Stack(
+              alignment: Alignment.center,
+            children:[
+            const SizedBox(
+              width: 45,
+              height: 45,
+                child: CircularProgressIndicator(
+                strokeWidth: 6,
+                value: 0.3,
+                color: Color.fromARGB(255, 62, 139, 236),
+                backgroundColor: Color.fromARGB(255, 17, 50, 74),
+              ),
+            ),
+          Text(
+                    '100 %',
+                    style: Theme.of(context).textTheme.bodySmall,
+                    textAlign: TextAlign.center,
+                  ),
+              ],
+        ),
+        ),
+      Text(
+        'Hoy',
+        style: Theme.of(context).textTheme.bodySmall,
+      ),
+       ],
+    );
+
+  }
+  Widget _smallDivider(){
+    return const VerticalDivider(
+        color: Color.fromARGB(255, 51, 51, 51),
+        thickness: 2,
+        width: 20,
+        indent: 10,
+        endIndent: 0,
+    );
+
+  }
+
+  Widget _hydration(){
+    return Column(
+      children: [
+        _intervalBar(),
+        Text(
+          'Hidratación',
+          style:Theme.of(context).textTheme.bodySmall,
+        ),
+      ],
+    );
+  }
+  Widget _intervalBar(){
+    return Text(
                   '1.0',
                   style: Theme.of(context).textTheme.headlineSmall,
-                ),
-    
-              ],
-            );
+                );
   }
   Widget _buttonReg(){
     return Center(
               child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                 
-                ),
                 onPressed:() { },
                 child: const Text('Registrar'),
                  ),
